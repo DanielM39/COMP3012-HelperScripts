c1fcc6e9235e6193c287e717a2fcc80babbe11b6
Daniel <46549393+DanielM39@users.noreply.github.com>
2023-11-14 21:33:53 -0800

    Fixed set-npm-shell.py

--- set-npm-shell.py
+++ set-npm-shell.py
@@ -1,7 +1,8 @@
 import re
 import subprocess
-from os import path
+from os import linesep, path
 from sys import platform
+from typing import List
 
 DEFAULT_PATH = "C:\\Program Files\\Git\\"
 DEFAULT_POWERSHELL_PATH = (
@@ -44,6 +45,35 @@ def get_pwsh_path() -> str:
     return CMD_PATH
 
 
+def write_npmrc(npmrc_path: str, lines: List[str]) -> bool:
+    file_lines: List[str]
+    if not path.exists(npmrc_path):
+        with open(npmrc_path, "w", encoding="utf8") as file:
+            for line in lines:
+                file.write(f"{line}{linesep}")
+        with open(npmrc_path, "r", encoding="utf8") as file:
+            lines.extend([""])
+            file_lines = [line.strip() for line in file.readlines()]
+            if file_lines == lines:
+                return True
+    else:
+        with open(npmrc_path, "r", encoding="utf8") as file:
+            file_lines = [file_line.strip() for file_line in file.readlines()]
+        with open(npmrc_path, "a", encoding="utf8") as file:
+            for line in lines:
+                if line not in file_lines:
+                    file.write(f"{line}{linesep}")
+        are_set: int = 0
+        with open(npmrc_path, "r", encoding="utf8") as file:
+            file_lines = [file_line.strip() for file_line in file.readlines()]
+            for line in lines:
+                if line in file_lines:
+                    are_set += 1
+        if are_set == len(lines):
+            return True
+    return False
+
+
 if __name__ == "__main__":
     if platform != "win32":
         print("This script is only needed on Windows.")
@@ -56,7 +86,12 @@ if __name__ == "__main__":
             else get_pwsh_path()
         )
         print(f"Shell:\t{shell_path}")
-        shell_path = shell_path.replace('\\', "\\\\")
-        cmd: [str] = ["npm", "config", "set", f'script-shell="{shell_path}"', "--userconfig", ".npmrc"]
-        print(cmd)
-        subprocess.run(cmd, shell=True)
+        shell_path = shell_path.replace("\\", "\\\\")
+        print(
+            "Done!"
+            if write_npmrc(
+                npmrc_path=path.join(".", ".npmrc"),
+                lines=[f'script-shell="{shell_path}"'],
+            )
+            else "ERROR: Could not set 'script-shell' in '.npmrc'."
+        )
